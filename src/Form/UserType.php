d57681f0c1107f6e398938f7a10200f059e4ec49
Hunter <98210343+Hunter1725@users.noreply.github.com>
2022-03-07 16:25:29 +0700

    hihi

---
+++ src/Form/UserType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('us_username')
+            ->add('us_password')
+            ->add('us_fullname')
+            ->add('string')
+            ->add('us_phone')
+            ->add('us_address')
+            ->add('us_role')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
